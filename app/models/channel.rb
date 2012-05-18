a46c646a7d082c7a08b2cb4520be563bc437a034
default_git_name <default_git_email>
2012-05-18 04:07:56 +0000

    Commented out the connect/disconnect code

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -4,17 +4,17 @@ class Channel < ActiveRecord::Base
 
   class << self
     def user_connect(user)
-      Channel.all.each do |channel|
-        activity = channel.activities.build(:user_id => user.id, :action => "connect")
-        activity.save
-      end
+      #Channel.all.each do |channel|
+       # activity = channel.activities.build(:user_id => user.id, :action => "connect")
+        #activity.save
+      #end
     end
 
     def user_disconnect(user)
-      Channel.all.each do |channel|
-        activity = channel.activities.build(:user_id => user.id, :action => "disconnect")
-        activity.save
-      end
+      #Channel.all.each do |channel|
+        #activity = channel.activities.build(:user_id => user.id, :action => "disconnect")
+        #activity.save
+      #end
     end
   end
 end
